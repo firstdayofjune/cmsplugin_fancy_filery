6428ba26e6f071a2de02d9cd579c6e0c3532ac5e
firstdayofjune <firstdayofjune@users.noreply.github.com>
2015-10-25 20:06:09 +0100

    Basic model for images and gallery plus tests

--- models.py
+++ models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from cms.models.pluginmodel import CMSPlugin
+from filer.fields.image import FilerImageField
+
+
+class Gallery(CMSPlugin):
+    title = models.TextField(default='Gallery')
+
+    def get_all_visible(self):
+        return self.image_set.filter(display=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Image(CMSPlugin):
+    filer = FilerImageField(verbose_name=_('image'), help_text=_('Please upload a jpeg or png image'), null=True)
+    title = models.TextField(default='Image')
+    description = models.TextField(null=True)
+    gallery = models.ForeignKey(Gallery, default=0)
+    gallery_position = models.IntegerField(default=0)
+    display = models.BooleanField(default=True)
+
+    def __str__(self):
+        return '{} ({})'.format(self.title, self.filer)
--- tests.py
+++ tests.py
@@ -1,3 +1,65 @@
 from django.test import TestCase
 
+from cmsplugin_fancy_filery.models import Image, Gallery
+from filer.models.imagemodels import Image as FilerImage
+
+
 # Create your tests here.
+class ImageAndGalleryModelTest(TestCase):
+
+    def test_creating_and_requesting_images(self):
+        filer1 = FilerImage()
+        filer1.save()
+        image = Image()
+        image.filer = filer1
+        image.title = 'This is the first image'
+        image.description = 'This first image has been taken, at a place yet unknown.'
+        image.save()
+
+        filer2 = FilerImage()
+        filer2.save()
+        image_wo_desc = Image()
+        image_wo_desc.filer = filer2
+        image_wo_desc.title = 'Image without description'
+        image_wo_desc.save()
+
+        saved_images = Image.objects.all()
+        self.assertEqual(len(saved_images), 2)
+
+        first_img = saved_images[0]
+        self.assertEqual(first_img.filer, filer1)
+        self.assertEqual(first_img.title, 'This is the first image')
+        self.assertEqual(first_img.description, 'This first image has been taken, at a place yet unknown.')
+
+        second_img = saved_images[1]
+        self.assertEqual(second_img.filer, filer2)
+        self.assertEqual(second_img.title, 'Image without description')
+        self.assertIsNone(second_img.description)
+
+
+    def test_gallery_contains_images(self):
+        gallery = Gallery()
+        gallery.title = 'My Gallery'
+        gallery.save()
+
+        saved_gallery = Gallery.objects.first()
+        self.assertEqual(saved_gallery, gallery)
+        self.assertEqual(saved_gallery.title, 'My Gallery')
+
+        image1 = Image()
+        image1.title = 'This is the first image'
+        image1.description = 'This first image has been taken, at a place yet unknown.'
+        image1.gallery = saved_gallery
+        image1.save()
+
+        image2 = Image()
+        image2.title = 'This is the second image'
+        image2.gallery = saved_gallery
+        image2.save()
+
+        gallery_images = saved_gallery.image_set.all()
+        print(saved_gallery.get_all_visible())
+        self.assertEqual(len(gallery_images), 2)
+        self.assertEqual(image1, gallery_images[0])
+        self.assertEqual(image2, gallery_images[1])
+
